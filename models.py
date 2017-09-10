f8043be9523c2c61654347e62fda76af55ae7121
Shikib Mehri <mehrishikib@gmail.com>
2017-09-09 20:26:21 -0700

    added encoder code

---
+++ models.py
@@ -0,0 +1,85 @@
+"""
+File which contains model definitions.
+"""
+
+class Encoder(nn.Module):
+  def __init__(
+    self,
+    input_size,
+    hidden_size,
+    vocab_size,
+    num_layers=1,
+    dropout=0,
+    bidirectional=True,
+    rnn_type='gru',
+  ):
+    """
+    Initialize the model.
+    """
+    super(Encoder, self).__init__()
+
+    self.num_directions = 2 if bidirectional else 1
+    self.vocab_size = vocab_size
+    self.input_size = input_size
+    self.hidden_size = hidden_size // self.num_directions
+    self.num_layers = num_layers
+    self.rnn_type = rnn_type
+
+    self.embedding = nn.Embedding(vocab_size, input_size, sparse=False, padding_idx=0)
+
+    if rnn_type == 'gru':
+      self.rnn = nn.GRU(
+        input_size,
+        self.hidden_size,
+        num_layers=num_layers,
+        dropout=dropout,
+        bidirectional=bidirectional,
+        batch_first=True,
+      ).cuda()
+    else:
+      self.rnn = nn.LSTM(
+        input_size,
+        self.hidden_size,
+        num_layers=num_layers,
+        dropout=dropout,
+        bidirectional=bidirectional,
+        batch_first=True,
+      ).cuda()
+
+    self.init_weights()
+
+  def forward(self, inps):
+    """
+    Forward pass for the model.
+
+    Input -> Embed -> RNN -> Output
+    """
+    embs = self.embedding(inps)
+    outputs, hiddens = self.rnn(embs)
+    return outputs, hiddens
+
+  def init_weights(self):
+    """
+    Initialize weights for RNN/embedding.
+    """
+    # Common initialization strategy
+    init.orthogonal(self.rnn.weight_ih_l0)
+    init.uniform(self.rnn.weight_hh_l0, a=-0.01, b=0.01)
+
+    # Pre-trained embeddings
+    pretrained_embeddings = preprocessing.load_embeddings()
+    embedding_weights = torch.FloatTensor(self.vocab_size, self.input_size)
+    init.uniform(embedding_weights, a=-0.25, b=0.25)
+    for k,v in pretrained_embeddings.items():
+      embedding_weights[k] = torch.FloatTensor(v)
+    embedding_weights[0] = torch.FloatTensor([0]*self.input_size)
+    del self.embedding.weight
+    self.embedding.weight = nn.Parameter(embedding_weights)
+
+
+class EncoderRNN(nn.Module):
+  def __init__(self, vocab_size, input_size, hidden_size, num_layers=1):
+    """
+    Initialize the model.
+    """
+
